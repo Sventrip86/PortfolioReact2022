04347c64ccec991946773e4419214ab86935fe46
Fabs <fab.sebis@google.com>
2022-10-29 11:43:21 +0200

    new pics

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from 'react-icons/fi';
 
 const about = () => {
   return (
-   <section id='about'>
-    <div className="about">
-    <h4>little bit</h4>
-      <h1>About me</h1>
-      
-    </div>
-   </section>
-    
-  )
-}
+    <section id="about">
+        <h5>little bit</h5>
+        <h2>About me</h2>
 
-export default about
+        <div className="container about__container">
+          <div className="about__me">
+            <div className="about__me__image">
+              <img src={ME} alt="me2" />
+            </div>
+        </div>
+
+        <div className="about__content">
+              <div className="about__cards">
+                <article className="about__card">
+                  <FaAward className="about__icon" />
+                  <h5>Experience</h5>
+                  <small>5 years</small>
+                </article>
+              
+              <article className="about__card">
+                <FiUsers className="about__icon" />
+                <h5>Projects</h5>
+                <small>5</small>
+              </article>
+          
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Clients</h5>
+              <small>50</small>
+            </article>
+            </div>  
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <a href="#contact" className='btn btn-primary'>Let's talk</a>
+        </div>  
+        </div>
+      
+  </section>
+  );
+};
+
+export default about;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
 import './portfolio.css'
+import ME from "../../assets/me.png";
+
+
+const data = [
+  {
+    id: 1,
+    image: ME,
+    title: 'lorem ipsum bla bla bla',
+    github: 'https://github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 2,
+    image: ME,
+    title: 'lorem ipsum bla bla bla',
+    github: 'https://github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 3,
+    image: ME,
+    title: 'lorem ipsum bla bla bla',
+    github: 'https://github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 4,
+    image: ME,
+    title: 'lorem ipsum bla bla bla',
+    github: 'https://github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 5,
+    image: ME,
+    title: 'lorem ipsum bla bla bla',
+    github: 'https://github.com',
+    demo: 'https://dribble.com',
+  },
+  {
+    id: 6,
+    image: ME,
+    title: 'lorem ipsum bla bla bla',
+    github: 'https://github.com',
+    demo: 'https://dribble.com',
+  }
+
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+       {
+        data.map(({id, image, title, github, demo}) => {
+            return(
+              <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+            <a href={github} className="btn">GitHub</a>
+            <a href={demo} className="btn btn-primary" target='_blank'>Live Demo</a>
+          </div>  
+        </article>
+            )
+        }
+        )   
+       }
+        
+      </div>  
+    </section>
   )
 }
 
